eeb0180a0fe80ce4502343658eee8a84e4a217bf
killpeoplewithhammers <123596850+killpeoplewithhammers@users.noreply.github.com>
2023-03-20 14:04:32 -0700

    sdfsadgnasdng

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
-import "github.com/gen2brain/raylib-go/raylib"
+import rl "github.com/gen2brain/raylib-go/raylib"
 
 func main() {
 	rl.InitWindow(800, 450, "raylib [core] example - basic window")
 
 	rl.SetTargetFPS(60)
 
+	titleHeight := 200
+
 	for !rl.WindowShouldClose() {
+
 		rl.BeginDrawing()
 
 		rl.ClearBackground(rl.RayWhite)
 
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+		rl.DrawText("Congrats! You created your first window!", 190, int32(titleHeight), 20, rl.LightGray)
 
 		rl.EndDrawing()
 	}
