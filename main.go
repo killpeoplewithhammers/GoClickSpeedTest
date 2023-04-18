dcc4d886decccabed94b0f9f75f1f189e184adb3
killpeoplewithhammers <123596850+killpeoplewithhammers@users.noreply.github.com>
2023-04-18 14:23:05 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"strconv"
 	"time"
 
@@ -10,10 +9,9 @@ import (
 )
 
 func main() {
+	rl.SetConfigFlags(rl.FlagWindowResizable)
 	rl.InitWindow(800, 450, "Click Test")
-	startTime := time.Now()
-	fmt.Println(startTime)
-
+	var click string
 	var clickSlice []time.Time
 
 	for !rl.WindowShouldClose() {
@@ -22,20 +20,29 @@ func main() {
 		if rl.IsMouseButtonPressed(0) {
 			clickSlice = append(clickSlice, currentTime)
 		}
+		if rl.IsMouseButtonPressed(1) {
+			clickSlice = append(clickSlice, currentTime)
+		}
 		now := time.Now()
 		if len(clickSlice) > 0 {
 			for i := 0; i <= len(clickSlice); i++ {
 				if now.Sub(clickSlice[0]) > 1000000000 {
 					clickSlice = slices.Delete(clickSlice, 0, 1)
 				}
-			} // */
+			}
+		}
+		if len(clickSlice) == 1 {
+			click = " click "
+		} else {
+			click = " clicks "
 		}
 		rl.ClearBackground(rl.RayWhite)
-		textSize := rl.MeasureText(strconv.Itoa(len(clickSlice))+" clicks per second", 20)
+		fontSize := int32(rl.GetRenderWidth() / 40)
+		textWidth := rl.MeasureText(strconv.Itoa(len(clickSlice))+click+"per second", fontSize)
+		textPositionX := int32(rl.GetRenderWidth()/2 - int(textWidth)/2)
 
-		textPositionX := 400 - textSize/2
-
-		rl.DrawText(strconv.Itoa(len(clickSlice))+" clicks per second", textPositionX, 200, 20, rl.Black)
+		textPositionY := int32(rl.GetRenderHeight())/2 - fontSize/2
+		rl.DrawText(strconv.Itoa(len(clickSlice))+click+"per second", textPositionX, textPositionY, fontSize, rl.Black)
 
 		rl.EndDrawing()
 	}
